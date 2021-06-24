d09b2cfda9cd0ba505f1282188dab8f9222b5b32
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-06-24 11:17:02 -0500

    Alta Almacenes-Se continua con Tipo Logistica

---
+++ Code/ServicesManagementCentral.Web/DAL/DALTipoLogistica.cs
@@ -0,0 +1,44 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ServicesManagement.Web.DAL
+{
+    public class DALTipoLogistica
+    {
+        public static DataSet up_CorpTMS_sel_TipoLogistica()
+        {
+
+            DataSet ds = new DataSet();
+
+            string conection = ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]];
+            if (System.Configuration.ConfigurationManager.AppSettings["flagConectionDBEcriptado"].ToString().Trim().Equals("1"))
+            {
+                conection = Soriana.FWK.FmkTools.Seguridad.Desencriptar(ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]]);
+            }
+
+
+            try
+            {
+                Soriana.FWK.FmkTools.SqlHelper.connection_Name(ConfigurationManager.ConnectionStrings["Connection_DEV"].ConnectionString);
+
+                System.Collections.Hashtable parametros = new System.Collections.Hashtable();
+
+                ds = Soriana.FWK.FmkTools.SqlHelper.ExecuteDataSet(CommandType.StoredProcedure, "[dbo].[up_CorpTMS_sel_TipoLogistica]", false, parametros);
+
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
---
+++ Code/ServicesManagementCentral.Web/Models/TipoLogistica/TipoLogisticaCns.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.Models.TipoLogistica
+{
+    public class TipoLogisticaCns
+    {
+        public int IdTipoLogistica { get; set; }
+        public string TipoLogistica { get; set; }
+        public decimal MinPesoVolumetrico { get; set; }
+        public decimal MaxPesoVolumetrico { get; set; }
+        public decimal MaxCosto { get; set; }
+        public string TipoArticulo { get; set; }
+        public string FechaCreacion { get; set; }
+        public string UsuarioCreacion { get; set; }
+        public string FechaUltModif { get; set; }
+        public string UsuarioUltModif { get; set; }
+
+    }
+}
