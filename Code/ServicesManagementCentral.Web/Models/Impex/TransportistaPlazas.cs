c4f8627d0dfff5ab89a3d89cb0427a8ce45c3d70
eliseo gijon romero <egijon@itechdev.com.mx>
2021-06-24 22:31:45 -0500

    ajustes de pantalla Cobertura de transportista

--- Code/ServicesManagementCentral.Web/Models/Impex/TransportistaPlazas.cs
+++ Code/ServicesManagementCentral.Web/Models/Impex/TransportistaPlazas.cs
@@ -17,13 +17,17 @@ namespace ServicesManagement.Web.Models.Impex
 
     public class TransportistaPlazasShow
     {
-        public string Transportista { get; set; }
-        public string Plaza { get; set; }
+        public string IdTransportista { get; set; }
+        public string NomTransportista { get; set; }
+        public int IdPlaza { get; set; }
+        public string CvePlaza { get; set; }
+        public string NomPlaza { get; set; }
         public string DescTipoEnvio { get; set; }
         public string PostalCode { get; set; }
         public string CreatedId { get; set; }
         public string CreatedDate { get; set; }
-
+        public string CreatedTime { get; set; }
+        public string BitActivo { get; set; }
     }
 
 }
