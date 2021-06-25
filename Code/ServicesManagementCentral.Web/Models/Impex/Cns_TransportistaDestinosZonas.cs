002386212239bdd3be19ab997298a87eb6185f54
eliseo gijon romero <egijon@itechdev.com.mx>
2021-06-25 06:01:53 -0500

    ajustes Cobertura por zonas

--- Code/ServicesManagementCentral.Web/Models/Impex/Cns_TransportistaDestinosZonas.cs
+++ Code/ServicesManagementCentral.Web/Models/Impex/Cns_TransportistaDestinosZonas.cs
@@ -8,10 +8,16 @@ namespace ServicesManagement.Web.Models.Impex
     public class Cns_TransportistaDestinosZonas
     {
         public int IdTransportista { get; set; }
+        public string NombreTransportista { get; set; }
         public string Cve_PlazaOrigen { get; set; }
+        public string NombrePlazaOrigen { get; set; }
         public string Cve_PlazaDestino { get; set; }
-        public string IdZona { get; set; }
+        public string NombrePlazaDestino { get; set; }
+        public int IdZona { get; set; }
+        public string NombreZona { get; set; }
         public string CreatedId { get; set; }
         public string CreatedDate { get; set; }
+        public string CreatedTime { get; set; }
+        public string BitActivo { get; set; }
     }
 }
