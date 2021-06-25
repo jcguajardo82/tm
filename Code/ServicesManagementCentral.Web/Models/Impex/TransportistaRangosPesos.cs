a5a83e1d3b9f902b6eef6535d28d9664c61e074f
eliseo gijon romero <egijon@itechdev.com.mx>
2021-06-25 05:22:42 -0500

    ajustes de Transportista Rangos Pesos

--- Code/ServicesManagementCentral.Web/Models/Impex/TransportistaRangosPesos.cs
+++ Code/ServicesManagementCentral.Web/Models/Impex/TransportistaRangosPesos.cs
@@ -18,11 +18,13 @@ namespace ServicesManagement.Web.Models.Impex
     public class TransportistaRangosPesosShow
     {
         public string IdTransportista { get; set; }
+        public string NombreTransportista { get; set; }
         public decimal PesoInicio { get; set; }
         public decimal PesoFin { get; set; }
         public decimal PorcentajeInicialCliente { get; set; }
-        public bool bitDeleted { get; set; }
         public string CreatedId { get; set; }
         public string CreatedDate { get; set; }
+        public string CreatedTime { get; set; }
+        public string bitActivo { get; set; }
     }
 }
