3e16b5ca65cf6a3600c4eb127c73da56622ab056
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-06-25 01:36:42 -0500

    Avance PromocionesCostoEnvio

--- Code/ServicesManagementCentral.Web/Controllers/PromocionesCostoEnvioController.cs
+++ Code/ServicesManagementCentral.Web/Controllers/PromocionesCostoEnvioController.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using ServicesManagement.Web.DAL;
+using ServicesManagement.Web.Helpers;
+using ServicesManagement.Web.Models.TipoLogistica;
+using System;
 using System.Web.Mvc;
 
 namespace ServicesManagement.Web.Controllers
@@ -17,6 +17,24 @@ namespace ServicesManagement.Web.Controllers
 
         public ActionResult GetPromociones() {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetTipoLogistica()
+        {
+            try
+            {
+
+                var listC = DataTableToModel.ConvertTo<TipoLogisticaCns>(DALTipoLogistica.up_CorpTMS_sel_TipoLogisticaByBit().Tables[0]);
+
+                var result = new { Success = true, resp = listC };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                var result = new { Success = false, Message = ex.Message };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
--- Code/ServicesManagementCentral.Web/DAL/DALTipoLogistica.cs
+++ Code/ServicesManagementCentral.Web/DAL/DALTipoLogistica.cs
@@ -40,5 +40,42 @@ namespace ServicesManagement.Web.DAL
             }
 
         }
+
+        public static DataSet up_CorpTMS_sel_TipoLogisticaByBit(bool BitActivo=true)
+        {
+
+            DataSet ds = new DataSet();
+
+            string conection = ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]];
+            if (System.Configuration.ConfigurationManager.AppSettings["flagConectionDBEcriptado"].ToString().Trim().Equals("1"))
+            {
+                conection = Soriana.FWK.FmkTools.Seguridad.Desencriptar(ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]]);
+            }
+
+
+            try
+            {
+                Soriana.FWK.FmkTools.SqlHelper.connection_Name(ConfigurationManager.ConnectionStrings["Connection_DEV"].ConnectionString);
+
+                System.Collections.Hashtable parametros = new System.Collections.Hashtable();
+                parametros.Add("@BitActivo", BitActivo);
+
+                ds = Soriana.FWK.FmkTools.SqlHelper.ExecuteDataSet(CommandType.StoredProcedure, "[dbo].[up_CorpTMS_sel_TipoLogisticaByBit]", false, parametros);
+
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
     }
 }
