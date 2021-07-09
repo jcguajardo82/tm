00a963cb8408798d0b7a45bd6e01484a53ee1fb0
eliseo gijon romero <egijon@itechdev.com.mx>
2021-07-09 01:18:20 -0500

    ajuste TipoServicio, mapeo de hora de actualizacion

--- Code/ServicesManagementCentral.Web/Models/Catalogos/TipoServicio.cs
+++ Code/ServicesManagementCentral.Web/Models/Catalogos/TipoServicio.cs
@@ -11,8 +11,10 @@ namespace ServicesManagement.Web.Models.Catalogos
         public string Desc_TipoServicio { get; set; }
         public string BitActivo { get; set; }
         public string CreateDate { get; set; }
+        public string CreateTime { get; set; }
         public string Created_User { get; set; }
         public string FecMovto { get; set; }
+        public string TimeMovto { get; set; }
         public string Modified_User { get; set; }
     }
 }
