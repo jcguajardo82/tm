c02a8e6e6e7ed42246d2ae31954780e1536f68fb
Pedro Castillo <agonzalez@itechdev.com.mx>
2022-02-21 10:48:51 -0600

    Estatus operativo transporte Mercancías Generales v1

---
+++ Code/ServicesManagementCentral.Web/Models/Dashboard/EstatusOP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.Models.Dashboard
+{
+    public class EstatusOP
+    {//  
+        public int TotalTransito { get; set; }
+        public int TotalPend { get; set; }
+        public int TotalEnt { get; set; }
+        public decimal PorcPend { get; set; }
+        public decimal PorcTransito { get; set; }
+        public decimal PorcEntregadas { get; set; }
+      
+    }
+}
