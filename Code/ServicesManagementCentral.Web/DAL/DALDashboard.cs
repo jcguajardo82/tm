a6ff0d996170aa044697056b1aa23d3bd8da52f3
Pedro Castillo <agonzalez@itechdev.com.mx>
2022-03-01 16:27:05 -0600

    DashBoard  Grafica V1

--- Code/ServicesManagementCentral.Web/DAL/DALDashboard.cs
+++ Code/ServicesManagementCentral.Web/DAL/DALDashboard.cs
@@ -59,6 +59,75 @@ namespace ServicesManagement.Web.DAL
 
         }
 
+        public static DataSet upCorpOms_Cns_GraphEnviosVsEstatus(DateTime? fechaini, DateTime? fechafin, int? IdTransportista, int? IdTipoEnvio, int? IdTipoServicio
+            , int? IdTipoLogistica,string TipoAlmacen,string Almacen,int? TipoFecha,int? Estatus)
+        {
+            DataSet ds = new DataSet();
+
+            string conection = ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]];
+            if (System.Configuration.ConfigurationManager.AppSettings["flagConectionDBEcriptado"].ToString().Trim().Equals("1"))
+            {
+                conection = Soriana.FWK.FmkTools.Seguridad.Desencriptar(ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]]);
+            }
+
+
+            try
+            {
+
+//@fechaini datetime = null,
+//@fechafin datetime = null,
+//@IdTransportista bigint = null,
+//@IdTipoEnvio int = null,
+//@IdTipoServicio int = null,
+//@IdTipoLogistica int = null,
+//@TipoAlmacen varchar(5) = null,
+//@Almacen varchar(100) = null,
+//@TipoFecha int = null, --1 - fecha creacion, 2 - fecha recoleccion, 3 - fecha entrega
+//@Estatus int = null-- 1 - EN_TRANSITO, 2 - ENTREGADO, 3 - AMBOS
+
+                Soriana.FWK.FmkTools.SqlHelper.connection_Name(ConfigurationManager.ConnectionStrings["Connection_DEv"].ConnectionString);
+
+                System.Collections.Hashtable parametros = new System.Collections.Hashtable();
+                parametros.Add("@fechaini", fechaini);
+                parametros.Add("@fechafin", fechafin);
+
+                if (IdTransportista != null & IdTipoEnvio != 0)
+                    parametros.Add("@IdTransportista", IdTransportista);
+                if (IdTipoEnvio != null & IdTipoEnvio != 0)
+                    parametros.Add("@IdTipoEnvio", IdTipoEnvio);
+                if (IdTipoServicio != null & IdTipoEnvio != 0)
+                    parametros.Add("@IdTipoServicio", IdTipoServicio);
+                if (IdTipoLogistica != null & IdTipoEnvio != 0)
+                    parametros.Add("@IdTipoLogistica", IdTipoLogistica);
+                if (!string.IsNullOrEmpty(TipoAlmacen))
+                    parametros.Add("@TipoAlmacen", TipoAlmacen);
+                if (!string.IsNullOrEmpty(Almacen))
+                    parametros.Add("@Almacen", Almacen);
+                if (TipoFecha != null & TipoFecha != 0)
+                    parametros.Add("@TipoFecha", TipoFecha);
+                if (Estatus != null & Estatus != 0)
+                    parametros.Add("@Estatus", Estatus);
+
+                ds = Soriana.FWK.FmkTools.SqlHelper.ExecuteDataSet(CommandType.StoredProcedure, "[dbo].[upCorpOms_Cns_GraphEnviosVsEstatus]", false, parametros);
+
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+
+
+
         public static DataSet upCorpTms_Cns_DashboardTrans()
         {
 
---
+++ Code/ServicesManagementCentral.Web/Models/Dashboard/EnviosVsEstatus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.Models.Dashboard
+{
+    public class EnviosVsEstatus
+    {
+        public List<DatosEnvio> Envios { get; set; }
+        public List<DatosEstatus> Estatus { get; set; }
+    }
+
+    public class DatosEnvio
+    {
+        public DateTime FechaEnvio { get; set; }
+        public int TotalEnvios { get; set; }
+
+        public string Anio { get; set; }
+        public string Mes { get; set; }
+        public string Dia { get; set; }
+        public string Valor { get; set; }
+    }
+
+    public class DatosEstatus
+    {
+        public DateTime FechaEstatus { get; set; }
+        public int TotalEstatus { get; set; }
+
+        public string Anio { get; set; }
+        public string Mes { get; set; }
+        public string Dia { get; set; }
+        public string Valor { get; set; }
+    }
+}
